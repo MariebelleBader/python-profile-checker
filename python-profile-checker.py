6ceff75eb511326e50f302c893d2adb26f79b6fa
mariebelle <mariebellebader10@gmail.com>
2025-06-02 10:29:12 -0400

    CHANGES

--- python-profile-checker.py
+++ python-profile-checker.py
@@ -4,9 +4,9 @@ GPA = float(input("Enter you GPA (on a scale 4.0): "))
 field = input ("Field of Interest:")
 graduated = input("have you graduated? ")
 
-if age < 25 and gpa >= 3.5 and graduated == "yes":
+if age < 25 and GPA >= 3.5 and graduated == "yes":
     print("You are eligible for a scholarship.")
-elif age < 30 and gpa >= 2.5:
+elif age < 30 and GPA >= 2.5:
     print("You are eligible for an internship.")
 else:
     print("Please apply again later.")
